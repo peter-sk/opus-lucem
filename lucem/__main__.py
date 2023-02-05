eaed2758ea377d2f8c566685c42a88ea8b673c17
Peter Schneider-Kamp <jps@ordbogen.com>
2023-02-05 12:37:52 +0100

    fixing

--- lucem/__main__.py
+++ lucem/__main__.py
@@ -4,7 +4,7 @@ from . import get_app
 
 if __name__ == "__main__":
      server_params = ["host", "port"]
-     app_args = {x:y for x,y in (arg.split("=") for arg in argv[1:]) if x not in server_params)}
+     app_args = {x:y for x,y in (arg.split("=") for arg in argv[1:]) if x not in server_params}
      server_args = {"host": '0.0.0.0', "port": 8000}
-     server_args.update({x:y for x,y in (arg.split("=") for arg in argv[1:] if x in server_params)})
+     server_args.update({x:y for x,y in (arg.split("=") for arg in argv[1:]) if x in server_params})
      get_app(**app_args).run(**server_args)
