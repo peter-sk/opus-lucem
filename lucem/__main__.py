80ae34c4d963ebd6a9d02d1809b6e04d16c8cd09
Peter Schneider-Kamp <jps@ordbogen.com>
2023-02-05 12:30:54 +0100

    CUDA is ready for prime time

--- lucem/__main__.py
+++ lucem/__main__.py
@@ -3,4 +3,8 @@ from sys import argv
 from . import get_app
 
 if __name__ == "__main__":
-     get_app(**{x:y for x,y in (arg.split("=") for arg in argv[1:])}).run(host='0.0.0.0',port=8000)
+     server_params = ["host", "port"]
+     app_args = {x:y for x,y in (arg.split("=") for arg in argv[1:]) if x not in server_params)}
+     server_args = {"host": '0.0.0.0', "port": 8000}
+     server_args.update({x:y for x,y in (arg.split("=") for arg in argv[1:] if x in server_params)})
+     get_app(**app_args).run(**server_args)
--- lucem/config.py
+++ lucem/config.py
@@ -1,5 +1,5 @@
-#LANGS = ["en", "da", "de", "es", "fi", "fr", "no", "ru", "sv", "zh", "ar"]
-LANGS = ["en", "da", "sv"]
+LANGS = ["en", "da", "de", "es", "fi", "fr", "no", "ru", "sv", "zh", "ar"]
+#LANGS = ["en", "da", "sv"]
 
 MODEL_TEMPLATES = [
     "Helsinki-NLP/opus-mt-tc-big-%s-%s",
